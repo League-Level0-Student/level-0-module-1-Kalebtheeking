dda2390e1964065a065fe9584694d8b611effef7
league <league@192.168.1.80>
2026-04-10 16:57:11 -0700

    ahahahh

--- src/_03_if_else/_1_unbirthday/UnBirthday.java
+++ src/_03_if_else/_1_unbirthday/UnBirthday.java
@@ -1,5 +1,15 @@
 package _03_if_else._1_unbirthday;
 
-public class UnBirthday {
+import javax.swing.JOptionPane;
 
-}
+public class UnBirthday {
+	public static void main(String[] args) {
+		String guess = JOptionPane.showInputDialog("what is your birthday?");
+		if (guess.equals("4/10/26")) {
+		JOptionPane.showMessageDialog(null,"Happy birthday ");
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"happy unbirthday!");
+	}
+}}
+
